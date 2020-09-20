b5007273aa57cc9aedaaf48f12020a6d1290ce2e
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-20 11:32:23 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,14 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
+  match_str = /\A[ぁ-んァ-ン一-龥]/
+  match_kana = /\A[ァ-ヶー－]+\z/
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:  'Full-width characters' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :first_name, format: { with: match_str, message: 'Full-width characters' }
+    validates :last_name, format: { with: match_str, message:  'Full-width characters' }
+    validates :first_name_kana, format: { with: match_kana, message: 'Full-width katakana characters' }
+    validates :last_name_kana, format: { with: match_kana, message: 'Full-width katakana characters' }
   end
 end
