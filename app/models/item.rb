2972646b4b52f4836a1b3a73802f33b9277d564b
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-21 15:36:34 +0900

    ActiveHash作成(配送料の負担)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  belongs_to_active_hash :pay_shipping
 end
---
+++ spec/factories/pay_shippings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :pay_shipping do
+    
+  end
+end
