d5acb396e99bc3a972345482ad859950f089938d
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-21 17:35:16 +0900

    ビュー、コントローラ カラム名修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
     if @item.save
-      redirect_to :index
+      redirect_to root_path
     else
       render :new
     end
@@ -21,7 +20,7 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(
-      :name, :introdunction, :price, :category_id, :condition_id,
-      :pay_shipping_id, :delivery_source_id, :delivery_source_id, :image).merge(user_id: current_user.id)
+      :name, :introduction, :price, :category_id, :condition_id,
+      :pay_shipping_id, :delivery_source_id, :preparation_day_id, :image).merge(user_id: current_user.id)
   end
 end
