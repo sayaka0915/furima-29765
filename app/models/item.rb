ab2b22e3f9c1bb437a68b23886f7d3367ebc44a7
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-22 16:13:37 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,27 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_source
   belongs_to_active_hash :preparation_day
   has_one_attached :image
+  belongs_to :user
+
+  price_regex = /\A[0-9]+\z/
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :price, format: { with: price_regex, message: 'Half-width number' }
+  end
+
+  with_options presence: true, numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :condition_id
+    validates :pay_shipping_id
+    validates :delivery_source_id
+    validates :preparation_day_id
+  end
+
+  validates :price, numericality: {
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: 'Out of setting range'
+  }
 end
