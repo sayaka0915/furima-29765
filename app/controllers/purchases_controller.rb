52d6f06f73b68b359843755df957e7e1ab34ef32
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-28 16:27:48 +0900

    余白等修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,6 @@
 class PurchasesController < ApplicationController
-  before_action :set_item, only:[:index, :create]
-  before_action :authenticate_user!, only:[:index]
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @purchase = PurchaseAddress.new
@@ -32,15 +32,15 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.permit(:token, :postal_code, :delivery_source_id, :city,
-      :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+                  :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-    amount: @item.price,
-    card: purchase_params[:token],
-    currency:'jpy'
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
     )
   end
 end
