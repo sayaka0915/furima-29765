edc95a12bde07541c05173e19fb5cff86097a0fa
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-30 11:09:25 +0900

    jsファイル編集

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -12,21 +12,22 @@ const pay = ()=> {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-    
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
+      console.log(status,response)
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById('charge-form');
         const tokenObj = `<input value=${token} type='hidden' name='token'>`;
         renderDom.insertAdjacentHTML('beforeend', tokenObj);
+        document.getElementById('card-number').removeAttribute('name');
+        document.getElementById('card-exp-month').removeAttribute('name');
+        document.getElementById('card-exp-year').removeAttribute('name');
+        document.getElementById('card-cvc').removeAttribute('name');
+  
+        document.getElementById('charge-form').submit();
+        document.getElementById('charge-form').reset();
       }
-      document.getElementById('card-number').removeAttribute('name');
-      document.getElementById('card-exp-month').removeAttribute('name');
-      document.getElementById('card-exp-year').removeAttribute('name');
-      document.getElementById('card-cvc').removeAttribute('name');
-
-      document.getElementById('charge-form').submit();
-      document.getElementById('charge-form').reset();
     })
   })
 }
