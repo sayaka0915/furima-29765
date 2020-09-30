972c5810038f751c0c76ff50203cbe7625db6d16
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-09-30 12:44:45 +0900

    deproy.rb 誤字修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,13 @@ lock '3.14.1'
 set :application, 'furima-29765'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sayaka0915/hurima-29765.git'
+set :repo_url,  'git@github.com:sayaka0915/furima-29765.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
