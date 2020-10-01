e7ea3a5c284687a3b7f5659d295c6017c09af342
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-01 15:56:58 +0900

    cable .yml編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://18.181.135.146//cable'
+config.action_cable.allowed_request_origins = [ 'http://18.181.135.146' ] 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
