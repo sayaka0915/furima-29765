b34d812733a7f4b41ec21d9e2de2d0197fdc5bfa
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-01 14:30:40 +0900

    Commentモデルテスト

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'コメント投稿機能' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it '全ての値が存在していれば登録できること' do
+      expect(@comment).to be_valid
+    end
+
+    it 'textが空では登録ができないこと' do
+      @comment.text = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Text can't be blank")
+    end
+  end
+end
