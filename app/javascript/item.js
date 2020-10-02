4777a3b3dbcb84a53c87aecae8566766859b1e9c
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-02 22:15:54 +0900

    jsファイル変更(該当ページ以外でのエラー表示を回避)

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,13 +1,15 @@
-function item (){
-  const itemPrice = document.getElementById('item-price');
-  itemPrice.addEventListener('keyup', () => {
-    const addPriceArea = document.getElementById('add-tax-price');
-    const profitArea = document.getElementById('profit');
-    const price = itemPrice.value;
-    const addPrice = price * 0.1;
-    const profit = price - addPrice;
-    addPriceArea.innerHTML = Math.floor(`${addPrice}`);
-    profitArea.innerHTML = Math.floor(`${profit}`);
-  })
-}
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  function item (){
+    const itemPrice = document.getElementById('item-price');
+    itemPrice.addEventListener('keyup', () => {
+      const addPriceArea = document.getElementById('add-tax-price');
+      const profitArea = document.getElementById('profit');
+      const price = itemPrice.value;
+      const addPrice = price * 0.1;
+      const profit = price - addPrice;
+      addPriceArea.innerHTML = Math.floor(`${addPrice}`);
+      profitArea.innerHTML = Math.floor(`${profit}`);
+    })
+  };
 window.addEventListener('load', item);
+};
