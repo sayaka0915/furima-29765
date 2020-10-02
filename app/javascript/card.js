e915f85ef0d3ba15756edee69b9b51dc3990c967
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-02 16:49:42 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -19,14 +19,14 @@ const pay = ()=> {
         const renderDom = document.getElementById('charge-form');
         const tokenObj = `<input value=${token} type='hidden' name='token'>`;
         renderDom.insertAdjacentHTML('beforeend', tokenObj);
-        document.getElementById('card-number').removeAttribute('name');
-        document.getElementById('card-exp-month').removeAttribute('name');
-        document.getElementById('card-exp-year').removeAttribute('name');
-        document.getElementById('card-cvc').removeAttribute('name');
-  
-        document.getElementById('charge-form').submit();
-        document.getElementById('charge-form').reset();
       }
+      document.getElementById('card-number').removeAttribute('name');
+      document.getElementById('card-exp-month').removeAttribute('name');
+      document.getElementById('card-exp-year').removeAttribute('name');
+      document.getElementById('card-cvc').removeAttribute('name');
+
+      document.getElementById('charge-form').submit();
+      document.getElementById('charge-form').reset();
     })
   })
 }
